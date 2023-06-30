fa01d575d905ea13652e5410479be7426f88d8d5
arturolumev <edwin.lume@tecsup.edu.pe>
2023-06-30 03:44:59 -0500

    feat: se agrego funcionalidad para el login y register

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const mysql = require('mysql');
 
+//Necesario para autenticar al usuario
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+//Middleware CORS
+const cors = require('cors');
+
 const app = express();
+
 const PORT = 8000; // Puedes cambiar el puerto si lo deseas
 
 // Configuración de la conexión a MySQL
@@ -33,6 +41,78 @@ app.use((req, res, next) => {
   next();
 });
 
+// Agregar el middleware CORS
+app.use(cors());
+
+//LOGIN
+
+// Ruta GET para obtener todos los usuarios
+app.get('/api/usuarios', (req, res) => {
+  const query = 'SELECT * FROM usuarios';
+
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al obtener los usuarios:', error);
+      res.status(500).json({ error: 'Ocurrió un error al obtener los usuarios' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Endpoint de autenticación
+app.post("/api/usuarios/login", (req, res) => {
+  const { correo, password } = req.body;
+
+  // Verificar las credenciales del usuario en la base de datos
+  const query = "SELECT * FROM usuarios WHERE correo = ? AND password = ?";
+  connection.query(query, [correo, password], (error, results) => {
+    if (error) {
+      console.error("Error al verificar las credenciales:", error);
+      res.status(500).json({ error: "Ocurrió un error al verificar las credenciales" });
+    } else if (results.length > 0) {
+      // Las credenciales son válidas, generamos un token de acceso
+      const accessToken = jwt.sign({ correo }, "secretKey");
+
+      // Enviar el token de acceso como respuesta
+      res.json({ accessToken });
+    } else {
+      // Las credenciales no son válidas, enviamos una respuesta con código de estado 401 Unauthorized
+      res.status(401).json({ error: "Credenciales inválidas" });
+    }
+  });
+});
+
+// Ruta POST para registrar un nuevo usuario
+app.post('/api/usuarios', (req, res) => {
+  const { nombre, correo, password } = req.body;
+
+  // Verificar si el usuario ya existe en la base de datos
+  const checkUserQuery = 'SELECT * FROM usuarios WHERE correo = ?';
+  connection.query(checkUserQuery, [correo], (error, results) => {
+    if (error) {
+      console.error('Error al verificar el usuario:', error);
+      res.status(500).json({ error: 'Ocurrió un error al verificar el usuario' });
+    } else if (results.length > 0) {
+      res.status(400).json({ error: 'El usuario ya existe' });
+    } else {
+      // Insertar el nuevo usuario en la base de datos
+      const insertUserQuery = 'INSERT INTO usuarios (nombre, correo, password) VALUES (?, ?, ?)';
+      const values = [nombre, correo, password];
+
+      connection.query(insertUserQuery, values, (error, result) => {
+        if (error) {
+          console.error('Error al registrar un usuario:', error);
+          res.status(500).json({ error: 'Ocurrió un error al registrar un usuario' });
+        } else {
+          res.json({ message: 'Usuario registrado exitosamente' });
+        }
+      });
+    }
+  });
+});
+
+//TRABAJADORES
 // Ruta GET para obtener datos desde la base de datos
 app.get('/api/trabajadores', (req, res) => {
   const query = 'SELECT * FROM trabajadores';
