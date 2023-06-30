d73812de2f499ffd5e4ef5a5d1e296c865a5ad60
arturolumev <edwin.lume@tecsup.edu.pe>
2023-06-30 03:54:44 -0500

    feat: se modifico para gestionar la autenticacion en la app

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import Navigation from "./src/components/Navigation";
+import {AuthProvider} from './src/context/AuthContext';
 
-function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/api/data')
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
-
+const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Proyecto React Native y Express</Text>
-      {data && <Text>{data.message}</Text>}
-    </View>
+    <AuthProvider>
+      <Navigation />
+    </AuthProvider>    
   );
-}
+};
 
 export default App;
