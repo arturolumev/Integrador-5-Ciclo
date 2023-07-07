3265e39b392962b50d277f5503c09a3ad47d72a0
arturolumev <edwin.lume@tecsup.edu.pe>
2023-07-07 16:40:57 -0500

    feat: agregacion de nueva vista para agregar horarios

---
+++ frontend/src/screens/AgregarHorarioScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BASE_URL } from '../config'
+import { Button } from 'react-native'
+import FormularioHorario from '../components/forms/FormularioHorario';
+
+function AgregarHorarioScreen({ navigation }) {
+  const url = BASE_URL;
+
+  const crearHorario = (nuevoHorario) => {
+    fetch(`${url}/horarios`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(nuevoHorario)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Horario creado:", data);
+        navigation.navigate('Horario', {refresh: true});
+      })
+      .catch((error) => console.log('Error al crear el horario:', error));
+  };
+
+  return (
+    <FormularioHorario agregarHorario={crearHorario} />
+  );
+}
+
+export default AgregarHorarioScreen;
