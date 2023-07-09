449b29c1a47337a857fe1928e687a4e247f21483
arturolumev <edwin.lume@tecsup.edu.pe>
2023-07-09 13:02:43 -0500

    feat: obtener areas

--- backend/server.js
+++ backend/server.js
@@ -541,9 +541,16 @@ app.delete('/api/horariosxentidad/:hxe_id', (req, res) => {
   });
 });
 
+// Ruta GET para obtener todos las areas
+app.get('/api/areas', (req, res) => {
+  const query = 'SELECT * FROM areas_are';
 
-
-
-
-
-
+  pool.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al obtener las areas:', error);
+      res.status(500).json({ error: 'Ocurrió un error al obtener las areas' });
+    } else {
+      res.json(results);
+    }
+  });
+});
