55413478a24844d734f40f30065991f1dd1e4662
arturolumev <edwin.lume@tecsup.edu.pe>
2023-07-09 04:12:29 -0500

    feat: se modifico el texto del boton segun el rol

--- frontend/src/screens/PerfilScreen.js
+++ frontend/src/screens/PerfilScreen.js
@@ -4,6 +4,7 @@ import { BASE_URL } from "../config";
 
 const PerfilScreen = ({ navigation, route }) => {
   const [perfil, setPerfil] = useState(null);
+  const [botonTexto, setBotonTexto] = useState('');
 
   useEffect(() => {
     obtenerPerfil();
@@ -13,6 +14,9 @@ const PerfilScreen = ({ navigation, route }) => {
     try {
       const token = localStorage.getItem("accessToken");
       const usuCodigo = route.params.usuCodigo;
+      const rol = route.params.rol;
+
+      console.log('codigo===>' + usuCodigo);
 
       const response = await fetch(`${BASE_URL}/usuarios/${usuCodigo}/perfil`, {
         headers: {
@@ -23,6 +27,15 @@ const PerfilScreen = ({ navigation, route }) => {
       if (response.ok) {
         const data = await response.json();
         setPerfil(data);
+        console.log('rol ===>' + rol);
+
+        // Determinar el texto del botón según el rol del usuario
+        if (rol === 'ADM') {
+          setBotonTexto('Ver Trabajadores');
+        } else if (rol === 'TRA') {
+          setBotonTexto('Ver mi Entidad');
+        }
+
       } else {
         console.log("Error al obtener el perfil del usuario");
       }
@@ -34,7 +47,7 @@ const PerfilScreen = ({ navigation, route }) => {
 
   return (
     <View style={styles.container}>
-      <Button onPress={() => navigation.navigate("Trabajador")} title="Ver Trabajadores" color="#841584" />
+      <Button onPress={() => navigation.navigate("Trabajador", { usuCodigo: route.params.usuCodigo, rol: route.params.rol })} title={botonTexto} color="#841584" />
       <Text>Perfil de Usuario</Text>
       {perfil && (
         <View>
