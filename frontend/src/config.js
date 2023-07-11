27d1c44cf5080c1d6a07b490905a091afda97f68
arturolumev <edwin.lume@tecsup.edu.pe>
2023-07-10 20:53:38 -0500

    feat: cambio de rutas con la api en aws y docker

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,7 +5,10 @@ const MY_IP = '';
 //export const BASE_URL = `http://${MY_IP}:8000/api`;
 
 // De manera local Postgres
-export const BASE_URL = `http://${MY_IP}:8000/api`;
+//export const BASE_URL = `http://${MY_IP}:8000/api`;
+
+// ngrok
+//export const BASE_URL = `https://fa43-38-250-130-206.ngrok-free.app/api`;
 
 // Usando AWS
-//export const BASE_URL = `http://ec2-34-195-50-74.compute-1.amazonaws.com:3000/api`;
+export const BASE_URL = `http://ec2-34-195-50-74.compute-1.amazonaws.com/api`;
